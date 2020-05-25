95e4458d87fc537dbc785c70328351b2f36cc42a
gosuna <gosuna79@gmail.com>
2020-05-24 19:29:14 -0600

    generador de carruseles

---
+++ obtenerCarruseles.js
@@ -0,0 +1,98 @@
+const puppeteer = require('puppeteer')
+var fs = require('fs')
+
+let items = [
+    '351636',
+    '343188',
+    '248008',
+    '237987',
+    '633852',
+    '627101',
+    '632295',
+    '423434',
+    '309869',
+    '553549'
+]
+let item = {
+    "index": "",
+    "sku": "PM-",
+    "url": "",
+    "image": {
+        "url": "",
+        "alt": ""
+    },
+    "texto": {
+        "titulo": "",
+        "copyContado": "Precio contado"
+    },
+    "precio": {
+        "precioAnterior": "",
+        "precioActual": "",
+        "precioSinFormato": ""
+    }
+}
+let json = {
+	"activo": true,
+	"titulo": "Lo que tu hogar necesita",
+	"items": [
+    ]
+}
+
+async function get () {
+    await puppeteer.launch({headless: true, args: ['--no-sandbox']}).then(async (browser) => {
+    for (var i = 0; i < items.length; i++) {
+        const page = await browser.newPage()
+        process.on("unhandledRejection", async (reason, p) => {
+            await page.waitFor(5000)
+            browser.close()
+        })
+        try {
+            console.log('generando el carrusel ' + i)
+            let url = 'https://www.coppel.com/SearchDisplay?categoryId=&storeId=12756&catalogId=10001&langId=-5&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex=0&pageSize=12&searchTerm=' + items[i]
+            await page.goto(url, { waitUntil: 'load'})
+            const element = await page.$("div.top.namePartPriceContainer.clearfix > div > h1")
+            let titulo = await page.evaluate(element => element.textContent, element)
+
+            const elementPrecioAnterior = await page.$("div > div.p_oferta > span:nth-child(2)")
+            let precioAnterior = await page.evaluate(element => element.textContent, elementPrecioAnterior)
+
+            const elementPrecioAhora = await page.$("div.p_oferta > div > div > span:nth-child(1)")
+            let precioAhora = await page.evaluate(element => element.textContent, elementPrecioAhora)
+
+            const imgSrc = await page.evaluate(
+                () => [...document.querySelectorAll('#Zoomer > figure > img')]
+                      .map(element => element.getAttribute('src'))
+            )
+            const imgAlt = await page.evaluate(
+                () => [...document.querySelectorAll('#Zoomer > figure > img')]
+                      .map(element => element.getAttribute('alt'))
+            )
+
+            item.index = i + 1 + ''
+            item.url = url
+            item.sku = item.sku + items[i]
+            item.texto.titulo = titulo.trim()
+            item.image.url = imgSrc[0].trim()
+            item.image.alt = imgAlt[0].trim()
+            item.precio.precioAnterior = precioAnterior.trim()
+            item.precio.precioActual = precioAhora.trim()
+            item.precio.precioSinFormato = precioAhora.trim().replace('$', '').replace(',', '')
+            
+            json.items.push(item)
+
+        }catch(error) {
+            console.log(error)
+            console.log('error al guardar el : ', items[i])
+        }finally {
+            await page.waitFor(500)
+            await page.close()
+        }
+    }
+    await browser.close()
+    return 'procesados'
+})
+fs.writeFileSync('carrusel.json', JSON.stringify(json))
+return true
+
+}
+get()
