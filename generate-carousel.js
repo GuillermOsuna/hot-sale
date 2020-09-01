861ff726ed97c8ddbfd9b26177bbbde16516f9c7
Julio Cesar <julio.rodriguez@coppel.com>
2020-09-01 10:46:03 -0600

    se cambio la estructura de los caruseles en el archivo generate

--- generate-carousel.js
+++ generate-carousel.js
@@ -8,90 +8,92 @@ let items = [
 ]
 
 let json = {
-	"activo": true,
-	"titulo": "Lo que tu hogar necesita",
-	"items": [
+    "activo": true,
+    "titulo": "Lo que tu hogar necesita",
+    "items": [
     ]
 }
 
-async function get () {
+async function get() {
     let itemsGeneral = []
-    await puppeteer.launch({headless: true, args: ['--no-sandbox']}).then(async (browser) => {
-    for (var i = 0; i < items.length; i++) {
-        const page = await browser.newPage()
-        process.on("unhandledRejection", async (reason, p) => {
-            await page.waitFor(5000)
-            browser.close()
-        })
-        try {
-            console.log('generando carrusel, item: ' + i)
-            let url = 'https://www.coppel.com/SearchDisplay?categoryId=&storeId=12756&catalogId=10001&langId=-5&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex=0&pageSize=12&searchTerm=' + items[i]
-            console.log('url: ', url)
-            await page.goto(url, { waitUntil: 'load'})
-            console.log('obteniendo datos.')
-            const element = await page.$("div.top.namePartPriceContainer.clearfix > div > h1")
-            let titulo = await page.evaluate(element => element.textContent, element)
+    await puppeteer.launch({ headless: true, args: ['--no-sandbox'] }).then(async (browser) => {
+        for (var i = 0; i < items.length; i++) {
+            const page = await browser.newPage()
+            process.on("unhandledRejection", async (reason, p) => {
+                await page.waitFor(5000)
+                browser.close()
+            })
+            try {
+                console.log('generando carrusel, item: ' + i)
+                let url = 'https://www.coppel.com/SearchDisplay?categoryId=&storeId=12756&catalogId=10001&langId=-5&sType=SimpleSearch&resultCatEntryType=2&showResultsPage=true&searchSource=Q&pageView=&beginIndex=0&pageSize=12&searchTerm=' + items[i]
+                console.log('url: ', url)
+                await page.goto(url, { waitUntil: 'load' })
+                console.log('obteniendo datos.')
+                const element = await page.$("div.top.namePartPriceContainer.clearfix > div > h1")
+                let titulo = await page.evaluate(element => element.textContent, element)
 
-            const elementPrecioAnterior = await page.$("div > div.p_oferta > span:nth-child(2)")
-            let precioAnterior = await page.evaluate(element => element.textContent, elementPrecioAnterior)
+                const elementPrecioAnterior = await page.$("div > div.p_oferta > span:nth-child(2)")
+                let precioAnterior = await page.evaluate(element => element.textContent, elementPrecioAnterior)
 
-            const elementPrecioAhora = await page.$("div.p_oferta > div > div > span:nth-child(1)")
-            let precioAhora = await page.evaluate(element => element.textContent, elementPrecioAhora)
+                const elementPrecioAhora = await page.$("div.p_oferta > div > div > span:nth-child(1)")
+                let precioAhora = await page.evaluate(element => element.textContent, elementPrecioAhora)
 
-            const imgSrc = await page.evaluate(
-                () => [...document.querySelectorAll('#Zoomer > figure > img')]
-                      .map(element => element.getAttribute('src'))
-            )
-            const imgAlt = await page.evaluate(
-                () => [...document.querySelectorAll('#Zoomer > figure > img')]
-                      .map(element => element.getAttribute('alt'))
-            )
-            let item = {
-                index: '',
-                sku: 'PR-',
-                url: '',
-                image: {
+                const imgSrc = await page.evaluate(
+                    () => [...document.querySelectorAll('#Zoomer > figure > img')]
+                        .map(element => element.getAttribute('src'))
+                )
+                const imgAlt = await page.evaluate(
+                    () => [...document.querySelectorAll('#Zoomer > figure > img')]
+                        .map(element => element.getAttribute('alt'))
+                )
+                let item = {
+                    sku: 'PR-',
                     url: '',
-                    alt: ''
-                },
-                texto: {
-                    titulo: '',
-                    copyContado: 'Precio contado'
-                },
-                precio: {
-                    precioAnterior: '',
-                    precioActual: '',
-                    precioSinFormato: ''
+                    posicion: '',
+                    image: {
+                        url: '',
+                        alt: ''
+                    },
+                    texto: {
+                        titulo: '',
+                        copyContado: 'Precio contado'
+                    },
+                    precio: {
+                        precioAnterior: '',
+                        precioActual: '',
+                        precioAnteriorSinSigno: '',
+                        precioActualSinSigno: ''
+                    }
                 }
+
+                item.posicion = i + 1 + ''
+                item.url = url
+                item.sku = item.sku + items[i]
+                item.texto.titulo = titulo.trim()
+                item.image.url = imgSrc[0].trim()
+                item.image.alt = imgAlt[0].trim()
+                item.precio.precioAnterior = precioAnterior.trim()
+                item.precio.precioActual = precioAhora.trim()
+                item.precio.precioAnteriorSinSigno = precioAnterior.trim().replace('$', '').replace(',', '')
+                item.precio.precioActualSinSigno = precioAhora.trim().replace('$', '').replace(',', '')
+
+                itemsGeneral.push(item)
+
+            } catch (error) {
+                console.log(error)
+                console.log('error al guardar el item: ', items[i])
+            } finally {
+                await page.waitFor(500)
+                await page.close()
             }
-
-            item.index = i + 1 + ''
-            item.url = url
-            item.sku = item.sku + items[i]
-            item.texto.titulo = titulo.trim()
-            item.image.url = imgSrc[0].trim()
-            item.image.alt = imgAlt[0].trim()
-            item.precio.precioAnterior = precioAnterior.trim()
-            item.precio.precioActual = precioAhora.trim()
-            item.precio.precioSinFormato = precioAhora.trim().replace('$', '').replace(',', '')
-            
-            itemsGeneral.push(item)
-
-        }catch(error) {
-            console.log(error)
-            console.log('error al guardar el item: ', items[i])
-        }finally {
-            await page.waitFor(500)
-            await page.close()
         }
-    }
 
-    json.items = itemsGeneral
-    await browser.close()
-    return 'procesados'
-})
-fs.writeFileSync('carrusel.json', JSON.stringify(json))
-return true
+        json.items = itemsGeneral
+        await browser.close()
+        return 'procesados'
+    })
+    fs.writeFileSync('carrusel.json', JSON.stringify(json))
+    return true
 
 }
 get()
